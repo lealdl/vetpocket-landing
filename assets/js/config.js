1106ddd32800b12bbe1a717a380955bea6a9d1c8
lealdl <lucianodleal@gmail.com>
2026-05-13 23:38:28 -0300

    fix: corrige URI absoluta e nome da imagem og

--- assets/js/config.js
+++ assets/js/config.js
@@ -5,10 +5,11 @@ const isLocal =
 const API_CONFIG = {
   BASE_URL: isLocal
     ? "http://127.0.0.1/backend-landing/"
-    : "https://llrh.com.br/backend-landing/",
+    : "https://llrh.com.br/backend-landing/", // Certifique-se de manter a barra final
 
   FETCH_OPTIONS: {
-    credentials: "include",
+    // credentials: "include" pode causar erros em requisições cross-origin (CORS)
+    // se o servidor não estiver configurado para domínios específicos.
     headers: {
       "Content-Type": "application/json",
     },
